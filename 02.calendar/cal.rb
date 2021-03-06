3e58b75695ee364365784b2d5b1d34698a62bb27
chidimi <daichi.imai@hotmail.com>
2021-03-07 02:44:51 +0900

    nowをtodayに変更した

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,10 +3,10 @@ require 'optparse'
 require 'date'
 
 params = ARGV.getopts('y:', 'm:')
-now = Date.today
+today = Date.today
 
-input_year = params["y"] ? params["y"].to_i : now.year
-input_month = params["m"] ? params["m"].to_i : now.month
+input_year = params["y"] ? params["y"].to_i : today.year
+input_month = params["m"] ? params["m"].to_i : today.month
 input_date = Date.new(input_year, input_month)
 input_date_last_date = Date.new(input_year, input_month, -1)
 
@@ -15,7 +15,7 @@ puts "日 月 火 水 木 金 土"
 
 input_date.upto(input_date_last_date){ |date|
   print "\s" * 3 * date.wday if date.day == 1
-  print "\e[30m\e[47m" if date == now
+  print "\e[30m\e[47m" if date == today
   print date.strftime("%e") + "\e[0m" + "\s"
   print "\n" if date.saturday?
 }
