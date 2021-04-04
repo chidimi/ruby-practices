3e7681415072cc8e91de39bacddbf029cf851434
chidimi <daichi.imai@hotmail.com>
2021-04-04 22:11:59 +0900

    formatを追加、add counterを追加、ARGV.delete_ifを削除

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -3,7 +3,7 @@
 
 require 'optparse'
 class Counter
-  attr_accessor :line_counter, :words_counter, :byte_counter
+  attr_reader :line_counter, :words_counter, :byte_counter
 
   def initialize(l_option, no_parameter)
     @line_counter = 0
@@ -14,18 +14,35 @@ class Counter
   end
 
   def show_counter(filename)
-    print @line_counter.to_s.rjust(8)
-    print @words_counter.to_s.rjust(8) unless @l_option
-    print @byte_counter.to_s.rjust(8) unless @l_option
+    print format(@line_counter)
+    unless @l_option
+      print format(@words_counter)
+      print format(@byte_counter)
+    end
     print " #{filename}" unless @no_parameter
     print "\n"
+  end
+
+  def add_line_counter(num)
+    @line_counter += num
+  end
+
+  def add_words_counter(num)
+    @words_counter += num
+  end
+
+  def add_byte_counter(num)
+    @byte_counter += num
+  end
+
+  def format(num)
+    num.to_s.rjust(8)
   end
 end
 
 def main
   params = ARGV.getopts('l')
   l_option = params['l']
-  ARGV.delete_if { |param| param == '-l' }
   total_flag = ARGV.size >= 2
   no_parameter = ARGV.size.zero?
   execute(l_option, no_parameter, total_flag)
@@ -35,15 +52,16 @@ def execute(l_option, no_parameter, total_flag)
   counter = Counter.new(l_option, no_parameter)
   total_counter = Counter.new(l_option, no_parameter)
   ARGF.each_line do |line|
-    counter.line_counter += 1
-    counter.words_counter += line.split(/\s+/).count { |str| str != '' }
-    counter.byte_counter += line.bytesize
+    counter.add_line_counter(1)
+    counter.add_words_counter(line.split(/\s+/).count { |str| str != '' })
+    counter.add_byte_counter(line.bytesize)
+
     next unless ARGF.eof?
 
     counter.show_counter(ARGF.filename)
-    total_counter.line_counter += counter.line_counter
-    total_counter.words_counter += counter.words_counter
-    total_counter.byte_counter += counter.byte_counter
+    total_counter.add_line_counter(counter.line_counter)
+    total_counter.add_words_counter(counter.words_counter)
+    total_counter.add_byte_counter(counter.byte_counter)
 
     counter = Counter.new(l_option, no_parameter)
   end
