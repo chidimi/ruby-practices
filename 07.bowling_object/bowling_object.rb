4a41a4eb272653475a7b728bc9a66d160bae0f7d
chidimi <daichi.imai@hotmail.com>
2021-06-19 02:14:33 +0900

    merge

--- 07.bowling_object/bowling_object.rb
+++ 07.bowling_object/bowling_object.rb
@@ -5,18 +5,17 @@ class Game
     shots_array = shots_text.split(',')
     @frames = Array.new(9) do
       shot = shots_array.shift
-      if shot == 'X'
-        Frame.new(shot)
-      else
-        Frame.new(shot, shots_array.shift)
-      end
+      @frames << if shot == 'X'
+                   Frame.new(shot)
+                 else
+                   Frame.new(shot, shots_array.shift)
+                 end
     end
     @frames << Frame.new(*shots_array)
   end
 
   def score
     total = 0
-<<<<<<< HEAD
     @current_frame_count = 0
 
     @frames.each do |frame|
@@ -43,20 +42,10 @@ class Game
   def strike_two_consecutive?
     @current_frame_count > 2 && @frames[@current_frame_count - 2].strike? && @frames[@current_frame_count - 3].strike?
   end
-=======
-    (0..9).each do |n|
-      frame, next_frame, after_next_frame = @frames.slice(n, 3)
-      total += frame.score(next_frame, after_next_frame)
-    end
-    total
-  end
->>>>>>> feature/ls_object
 end
 
 class Frame
   attr_reader :first_shot, :second_shot, :third_shot
-
-  PERFECT = 10
 
   def initialize(first_mark, second_mark = 0, third_mark = nil)
     @first_shot = Shot.new(first_mark)
@@ -64,32 +53,29 @@ class Frame
     @third_shot = Shot.new(third_mark)
   end
 
-  def score(next_frame, after_next_frame)
-    total = 0
-    total += @first_shot.score + @second_shot.score + @third_shot.score
-    total += next_frame.first_shot.score + next_frame.second_shot.score if !next_frame.nil? && strike?
-    total += next_frame.first_shot.score if !next_frame.nil? && spare?
-    total += after_next_frame.first_shot.score if !after_next_frame.nil? && strike? && next_frame.strike?
-    total
+  def score
+    @first_shot.score + @second_shot.score + @third_shot.score
   end
 
   def strike?
-    @first_shot.score == PERFECT
+    @first_shot.score == 10
   end
 
   def spare?
-    !strike? && (@first_shot.score + @second_shot.score) == PERFECT
+    !strike? && (@first_shot.score + @second_shot.score) == 10
   end
 end
 
 class Shot
+  attr_reader :mark
+
   def initialize(mark)
     @mark = mark
   end
 
   def score
-    return 10 if @mark == 'X'
+    return 10 if mark == 'X'
 
-    @mark.to_i
+    mark.to_i
   end
 end
